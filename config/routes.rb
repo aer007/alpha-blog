0da75f21ad00c57247c407df7b31f4fc9235d170
Amanda Robinson <aackley88@gmail.com>
2018-01-05 14:58:27 +0000

    UI sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   
   get 'pages/home' => 'pages#home'
   get 'pages/about' => 'pages#about'
-  
+  get 'signup' => 'users#new'
+  resources :users, except: [:new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
